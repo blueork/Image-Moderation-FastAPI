45f663307e5dae94948ba4cbabff9de4f286eac3
blueork <114741623+blueork@users.noreply.github.com>
2025-05-27 20:47:16 +0500

    Setup a basic Image Upload Endpoint

--- backend-fastAPI/main.py
+++ backend-fastAPI/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import io
+import json
+import uuid
 
 app = FastAPI()
 
@@ -10,3 +14,12 @@ async def root():
 @app.get("/check")
 def check():
     return "Hello World"
+
+@app.post("/detect-image")
+async def detectImage(file: UploadFile = File(...)):
+    # data = json.load(file.read())
+    # file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+
+    return {"filename": file.filename}
